848022bfb926e37ca223e2295c70a1401692dd6c
vaibhav sable <vaibhavsable52@gmail.com>
2022-05-02 21:35:58 -0700

    Emp present/Absent

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Emp
+ {
+    public static void Main(String[] args)
+    {
+        int Full_Time = 1;
+        Random r = new Random();
+        int empcheck = r.Next(0 , 2);
+        if (empcheck == Full_Time)
+        {
+            Console.WriteLine("Emp is presnt");
+
+        }
+        else
+        {
+            Console.WriteLine("Emp is absent");
+        }
+        
+    }
+  }
